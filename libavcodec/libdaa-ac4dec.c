478c7e51c13576d93ec057094083047841d5128e
Jason Justman <jjustman@sinclairdigital.com>
2020-09-16 11:30:55 -0700

    debugging

--- libavcodec/libdaa-ac4dec.c
+++ libavcodec/libdaa-ac4dec.c
@@ -1093,7 +1093,7 @@ static int daa_ac4_decode_frame(AVCodecContext *avctx, void *data,
     *got_frame_ptr = 1;
 
     //ret = avpkt->size - bytesconsumed;
-    ret = avpkt->size - bytesconsumed;
+    ret = bytesconsumed;
 
     frame->pkt_duration = av_rescale_q(frame->nb_samples, (AVRational){1, avctx->sample_rate},
             avctx->time_base);
